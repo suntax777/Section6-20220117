57e4163e195aac612d4720884b026e6a9a124f07
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-21 08:02:38 +0900

    0121

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecodaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
