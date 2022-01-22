cf65b785c3ddc8cf78dede39c204557bc999f8b2
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-22 22:11:20 +0900

    31-Finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecodaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "hiro",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "454545@fssss.com",
+  phone: "015-01587-8777",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "http://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
--- src/components/molecules/SearchInput.jsx
+++ src/components/molecules/SearchInput.jsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Input } from "../atoms/input/Input";
 
@@ -5,7 +7,18 @@ export const SearchInput = () => {
   return (
     <div>
       <Input placeholder="検索条件を入力" />
-      <PrimaryButton>検索</PrimaryButton>
+      <SButtonWrapper>
+        <PrimaryButton>検索</PrimaryButton>
+      </SButtonWrapper>
     </div>
   );
 };
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrapper = styled.div`
+  paddig-left: 8px;
+`;
